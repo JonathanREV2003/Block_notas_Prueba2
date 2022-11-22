08ff76eddaaa237ced9e08a29b6a13df6bab1154
Jonathan Ramiro Elías Vásquez <99297546+JonathanREV2003@users.noreply.github.com>
2022-11-21 21:16:34 -0600

    Initial commit

---
+++ app/src/main/java/com/example/blocdenotasPrueba/ui/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.blocdenotasPrueba.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.blocdenotasPrueba.R;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnllogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnllogin = findViewById(R.id.buttonlogin);
+
+        btnllogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, DashboardActivity.class);
+                startActivity(i);
+               
+            }
+        });
+    }
+
+}
